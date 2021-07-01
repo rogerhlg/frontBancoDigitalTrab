970a6bfe5f8caa74f22606ede3ebcc2087cc302c
Roger Henrique <rogerhlg@outlook.com>
2021-07-01 00:08:26 -0300

    Cadastrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarContaComponent } from './components/views/conta/cadastrar-conta/cadastrar-conta.component';
 import { ListarContaComponent } from './components/views/conta/listar-conta/listar-conta.component';
 
 const routes: Routes = [
@@ -7,6 +8,10 @@ const routes: Routes = [
     path: '',
     component: ListarContaComponent
   },
+  {
+    path: 'cadastrar/conta',
+    component: CadastrarContaComponent
+  }
 ];
 
 @NgModule({
--- src/app/components/views/conta/cadastrar-conta/cadastrar-conta.component.ts
+++ src/app/components/views/conta/cadastrar-conta/cadastrar-conta.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Conta } from 'src/app/models/Conta';
+import { Pix } from 'src/app/models/Pix';
+import { ContaService } from 'src/app/services/conta.service';
 
 @Component({
   selector: 'app-cadastrar-conta',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarContaComponent implements OnInit {
 
-  constructor() { }
+  data!: string;
+  tipo!: string;
+  chave!: string;
+
+  cpf!: string;
+  pix!: Pix;
+  sobrenome!: string;
+  nome!: string;
+  saldo!: number;
+  telefone!: string;
+
+  constructor(private service: ContaService, private router: Router, private snack: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
+  cadastrar(): void {
+
+    let pix = new Pix();
+    pix.tipo = this.tipo;
+    pix.chave = this.chave;
+
+    let conta = new Conta();
+    conta.primeiroNome = this.nome;
+    conta.CPF = this.cpf;
+    conta.sobrenome = this.sobrenome;
+    conta.pix = pix;
+    conta.telefone = this.telefone;
+    console.log(conta);
+    this.service.cadastrar(conta).subscribe(conta => {
+      console.log(conta);
+      this.snack.open("Conta digital cadastrada", "", {
+        duration: 3000,
+        horizontalPosition: "right",
+        verticalPosition: "top",
+      });
+      this.router.navigate([""]);
+    });
+  }
 }
